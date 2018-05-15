49866058ef5a1852b583fbce1ea40ee07aa86936
Nikita Rosenberg <rosenberg@magora-systems.com>
2018-05-15 15:55:57 +0700

    Add TableViewController and transition to it

--- Example/LEONetworkLayer/Workflows/Main/MainViewController.swift
+++ Example/LEONetworkLayer/Workflows/Main/MainViewController.swift
@@ -1,16 +1,17 @@
 import UIKit
 
-class MainViewController: UIViewController {
 
+
+class MainViewController: UITableViewController {
+
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+ 
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 }
 
---
+++ Example/LEONetworkLayer/Workflows/Main/TableViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+
+
+
+class TableViewController: UIViewController {
+    
+    
+}
